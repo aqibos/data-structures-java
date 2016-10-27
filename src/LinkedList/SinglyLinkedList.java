f57debe3b4b3a164afc731323fbf3b8da0e4395c
Aqib Shah <ashah023@fiu.edu>
2016-10-27 00:02:41 -0400

    implement more singly linked list methods

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -1,35 +1,63 @@
 package LinkedList;
 
 public class SinglyLinkedList implements LinkedList {
+	private ListNode head;
+	private int size;
 
-	@Override
-	public boolean add(ListNode node) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public SinglyLinkedList(){
+		this.size = 0;
 	}
 
 	@Override
-	public void add(int index, ListNode node) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public boolean add(int n) {
+		add(size, n);
+		return true;
 	}
 
 	@Override
-	public void addFirst(ListNode node) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public void add(int index, int n) {
+		if (index > this.size) return;
+
+		ListNode p = this.head;
+		ListNode toAdd = new ListNode(n);
+
+		if (this.isEmpty() || index == 0) {
+			this.head = toAdd;
+			this.head.setNext(p);
+		} else {
+			int count = 1;
+			while (p != null) {
+				if (count == index) {
+					ListNode tmp = p.getNext();
+					p.setNext(toAdd);
+					toAdd.setNext(tmp);
+				}
+				count++;
+				p = p.getNext();	
+			}
+		}
+		size++;
 	}
 
 	@Override
-	public void addLast(ListNode node) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public void addFirst(int n) {
+		add(0, n);
+	}
+
+	@Override
+	public void addLast(int n) {
+		add(size, n);
 	}
 
 	@Override
 	public void clear() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		this.head = null;
+		size = 0;
 	}
 
 	@Override
 	public boolean contains(int n) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return indexOf(n) >= 0;
 	}
 
 	@Override
@@ -47,9 +75,21 @@ public class SinglyLinkedList implements LinkedList {
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
+	// -1, if not foundd
 	@Override
 	public int indexOf(int n) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		ListNode p = this.head;
+		int index = -1;
+		int count = 0;
+		while (p != null) {
+			if (p.getData() == n) {
+				index = count; break;
+			}
+
+			p = p.getNext();
+			count++;
+		}
+		return index;
 	}
 
 	@Override
@@ -74,7 +114,27 @@ public class SinglyLinkedList implements LinkedList {
 
 	@Override
 	public int size() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	@Override
+	public String toString() {
+		String listAsString = "["; 
+		ListNode p = this.head;
+		while (p != null) {
+			listAsString += p.getData() + ", ";  // TODO: remove trailing comma
+			p = p.getNext();
+		}
+		listAsString += "]"; 
+		return listAsString;
+	}
+
+	public void printString() {
+		System.out.println(this.toString());	
 	}
 
 	@Override
